1db42e9f01e2383a608034aa0f54729432ea4d90
mawni <mawni@gmail.com>
2016-04-27 16:39:41 -0700

    fix void tags without attr regex bug

--- gulp-bracks.js
+++ gulp-bracks.js
@@ -212,10 +212,32 @@ const START_TAGS_WITHOUT_ATTR = {
 };
 
 /**
+ * void tags without attributes regular expressions object mapping
+ * @private
+ */
+const VOID_TAGS_WITHOUT_ATTR = {
+  '<area>': /(?:\[\barea\b\])/g,
+  '<base>': /(?:\[\bbase\b\])/g,
+  '<br>': /(?:\[\bbr\b\])/g,
+  '<col>': /(?:\[\bcol\b\])/g,
+  '<embed>': /(?:\[\bembed\b\])/g,
+  '<hr>': /(?:\[\bhr\b\])/g,
+  '<img>': /(?:\[\bimg\b\])/g,
+  '<input>': /(?:\[\binput\b\])/g,
+  '<keygen>': /(?:\[\bkeygen\b\])/g,
+  '<link>': /(?:\[\blink\b\])/g,
+  '<meta>': /(?:\[\bmeta\b\])/g,
+  '<param>': /(?:\[\bparam\b\])/g,
+  '<source>': /(?:\[\bsource\b\])/g,
+  '<track>': /(?:\[\btrack\b\])/g,
+  '<wbr>': /(?:\[\bwbr\b\])/g
+};
+
+/**
  * start and void tags with attributes regular expressions object mapping
  * @private
  */
-const START_TAGS_WITH_ATTR = {
+const START_VOID_TAGS_WITH_ATTR = {
   '<a ': /(?:\ba\b\()/g,
   '<abbr ': /(?:\babbr\b\()/g,
   '<address ': /(?:\baddress\b\()/g,
@@ -326,41 +348,19 @@ const START_TAGS_WITH_ATTR = {
 };
 
 /**
- * void tags without attributes regular expressions object mapping
- * @private
- */
-const VOID_TAGS_WITHOUT_ATTR = {
-  '<area>': /(?:\[\barea\b\])/g,
-  '<base>': /(?:\[\bbase\b\])/g,
-  '<br>': /(?:\[\bbr\b\])/g,
-  '<col>': /(?:\[\bcol\b\])/g,
-  '<embed>': /(?:\[\bembed\b\])/g,
-  '<hr>': /(?:\[\bhr\b\])/g,
-  '<img>': /(?:\[\bimg\b\])/g,
-  '<input>': /(?:\[\binput\b\])/g,
-  '<keygen>': /(?:\[\bkeygen\b\]])/g,
-  '<link>': /(?:\[\blink\b\]])/g,
-  '<meta>': /(?:\[\bmeta\b\]])/g,
-  '<param>': /(?:\[\bparam\b\]])/g,
-  '<source>': /(?:\[\bsource\b\]])/g,
-  '<track>': /(?:\[\btrack\b\]])/g,
-  '<wbr>': /(?:\[\bwbr\b\]])/g
-};
-
-/**
  * ejs specific tags regular expressions object mapping
  * @private
  */
 const EJS_TAGS = {
   '<%': /(?:\[%)/g,
-  '<%=': /(?:\[%=)/g,
-  '<%-': /(?:\[%\-)/g,
-  '<%#': /(?:\[%#)/g,
-  '<%%': /(?:\[%%)/g,
+  '<%=': /(?:%=)/g,
+  '<%-': /(?:%\-)/g,
+  '<%#': /(?:%#)/g,
+  '<%%': /(?:%%)/g,
   '%>': /(?:%\])/g,
-  '-%>': /(?:\-%\])/g,
-  '<%_': /(?:\[%_)/g,
-  '_%>': /(?:_%\])/g
+  '-%>': /(?:\-%)/g,
+  '<%_': /(?:%_)/g,
+  '_%>': /(?:_%)/g
 };
 
 /**
@@ -388,8 +388,8 @@ module.exports = function() {
     Object.keys(START_TAGS_WITHOUT_ATTR).forEach(function(key) {
       src = src.replace(START_TAGS_WITHOUT_ATTR[key], key);
     });
-    Object.keys(START_TAGS_WITH_ATTR).forEach(function(key) {
-      src = src.replace(START_TAGS_WITH_ATTR[key], key);
+    Object.keys(START_VOID_TAGS_WITH_ATTR).forEach(function(key) {
+      src = src.replace(START_VOID_TAGS_WITH_ATTR[key], key);
     });
     Object.keys(EJS_TAGS).forEach(function(key) {
       src = src.replace(EJS_TAGS[key], key);
